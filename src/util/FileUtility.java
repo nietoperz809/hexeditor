b1c4cdaa64b4ad35868907be197cf178dfbccbd6
pittbull <pbull809@gmail.com>
2015-01-01 18:11:54 +0100

    with splitpane

--- src/util/FileUtility.java
+++ src/util/FileUtility.java
@@ -7,11 +7,23 @@ package util;
 
 import java.awt.Component;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintStream;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
+
+interface LoadFunc
+{
+    String doIt(File file);
+}
+
+interface SaveFunc
+{
+    void doIt(String content, String name);
+}
 
 /**
  *
@@ -19,6 +31,32 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  */
 public class FileUtility
 {
+    private static String load(String s1, String s2, Component parent, LoadFunc code) throws Exception
+    {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setApproveButtonText("Get it!");
+        chooser.setFileFilter(new FileNameExtensionFilter(s1, s2));
+        if (chooser.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION)
+        {
+            String name = chooser.getSelectedFile().getAbsolutePath();
+            File file = new File(name); //for ex foo.txt
+            return code.doIt(file);
+        }
+        return null;
+    }
+
+    private static void save(String s1, String s2, Component parent, String content, SaveFunc code) throws Exception
+    {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setApproveButtonText("Save now!");
+        chooser.setFileFilter(new FileNameExtensionFilter(s1, s2));
+        if (chooser.showSaveDialog(parent) == JFileChooser.APPROVE_OPTION)
+        {
+            String name = chooser.getSelectedFile().getAbsolutePath();
+            code.doIt(content, name);
+        }
+    }
+
     /**
      *
      * @param parent
@@ -27,23 +65,20 @@ public class FileUtility
      */
     public static String loadSource(Component parent) throws Exception
     {
-        JFileChooser chooser = new JFileChooser();
-        chooser.setApproveButtonText("Get it!");
-        chooser.setFileFilter(new FileNameExtensionFilter("6502 ASM code", "asm"));
-        if (chooser.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION)
+        return load("6502 ASM code", "asm", parent, (File file) ->
         {
-            String name = chooser.getSelectedFile().getAbsolutePath();
-            File file = new File(name); //for ex foo.txt
-            String content;
+            String content = null;
             try (FileReader reader = new FileReader(file))
             {
                 char[] chars = new char[(int) file.length()];
                 reader.read(chars);
                 content = new String(chars);
             }
+            catch (IOException ex)
+            {
+            }
             return content;
-        }
-        return null;
+        });
     }
 
     /**
@@ -54,16 +89,15 @@ public class FileUtility
      */
     public static void saveSource(Component parent, String content) throws Exception
     {
-        JFileChooser chooser = new JFileChooser();
-        chooser.setApproveButtonText("Save now!");
-        chooser.setFileFilter(new FileNameExtensionFilter("6502 ASM code", "asm"));
-        if (chooser.showSaveDialog(parent) == JFileChooser.APPROVE_OPTION)
+        save ("6502 ASM code", "asm", parent, content, (String content1, String name) ->
         {
-            String name = chooser.getSelectedFile().getAbsolutePath();
             try (PrintStream out = new PrintStream(new FileOutputStream(name)))
             {
-                out.print(content);
+                out.print(content1);
             }
-        }
+            catch (FileNotFoundException ex)
+            {
+            }
+        });
     }
 }
